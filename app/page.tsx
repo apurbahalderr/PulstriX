dd8510d0f947ded640f72d18b7b0c41a5083e32f
ttldaemon <anshukr95086@gmail.com>
2026-08-02 06:27:36 +0530

    stats commmented

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
-      <Stats />
+      {/* <Stats /> */}
 
       <EmergencySection />
 
